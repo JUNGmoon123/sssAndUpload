ed4b1c5242c823ec7c5757ac53deed0adf124071
JUNGmoon123 <jungpro567@gmail.com>
2024-05-20 16:45:19 +0900

    로그인 방식을 스프링 시큐리티로 변경

--- src/main/java/com/koreait/sssandupload/app/base/SecurityConfig.java
+++ src/main/java/com/koreait/sssandupload/app/base/SecurityConfig.java
@@ -1,9 +1,12 @@
+
 package com.koreait.sssandupload.app.base;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,5 +18,10 @@ public class SecurityConfig {
                 .csrf().disable()
                 .authorizeRequests().antMatchers("/**").permitAll();
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
