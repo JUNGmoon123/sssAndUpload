95adcd36ae433cc48a0d1c974e36cd70bb5a348c
JUNGmoon123 <jungpro567@gmail.com>
2024-05-22 14:49:45 +0900

    컨트롤러에서 현재 로그인 한 회원의 정보를 MemberContext 객체를 통해 얻는 방법

--- src/main/java/com/koreait/sssandupload/app/home/contoller/HomeController.java
+++ src/main/java/com/koreait/sssandupload/app/home/contoller/HomeController.java
@@ -1,9 +1,15 @@
 package com.koreait.sssandupload.app.home.contoller;
 
 import com.koreait.sssandupload.app.member.service.MemberService;
+import com.koreait.sssandupload.app.security.dto.MemberContext;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,5 +24,17 @@ public class HomeController {
     @GetMapping("/about")
     public String showAbout() {
         return "home/about";
+    }
+
+    @GetMapping("/currentUserOrigin")
+    @ResponseBody
+    public Principal currentUserOrigin(Principal principal) {
+        return principal;
+    }
+
+    @GetMapping("/currentUser")
+    @ResponseBody
+    public MemberContext currentUser(@AuthenticationPrincipal MemberContext memberContext) {
+        return memberContext;
     }
 }
