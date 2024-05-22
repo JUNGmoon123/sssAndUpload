914395ed5c40492234ba6770f53fe549731fef7c
JUNGmoon123 <jungpro567@gmail.com>
2024-05-22 14:37:06 +0900

    모든 액션마다 작성한 현재 로그인 한 회원의 정보 가져오는 로직 제거, 이미지 누락

---
+++ src/main/java/com/koreait/sssandupload/app/home/contoller/HomeController.java
@@ -0,0 +1,22 @@
+package com.koreait.sssandupload.app.home.contoller;
+
+import com.koreait.sssandupload.app.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+    private final MemberService memberService;
+
+    @GetMapping("/")
+    public String showMain() {
+        return "home/main";
+    }
+
+    @GetMapping("/about")
+    public String showAbout() {
+        return "home/about";
+    }
+}
