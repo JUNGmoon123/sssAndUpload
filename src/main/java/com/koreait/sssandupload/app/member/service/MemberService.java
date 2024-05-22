d65a6c22dc2ed3b04e9a15cf64ff10642e9ccfde
JUNGmoon123 <jungpro567@gmail.com>
2024-05-22 14:13:28 +0900

    저장되는 파일의 확장자 유지, 저장 폴더를 날짜로 세분화

--- src/main/java/com/koreait/sssandupload/app/member/service/MemberService.java
+++ src/main/java/com/koreait/sssandupload/app/member/service/MemberService.java
@@ -1,8 +1,8 @@
-
 package com.koreait.sssandupload.app.member.service;
 
 import com.koreait.sssandupload.app.member.entity.Member;
 import com.koreait.sssandupload.app.member.repository.MemberRepository;
+import com.koreait.sssandupload.app.util.Util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
@@ -33,8 +33,11 @@ public class MemberService implements UserDetailsService {
     }
 
     public Member join(String username, String password, String email, MultipartFile profileImg) {
-        String profileImgDirName = "member";
-        String fileName = UUID.randomUUID().toString() + ".png";
+        String profileImgDirName = "member/" + Util.date.getCurrentDateFormatted("yyyy_MM_dd");
+
+        String ext = Util.file.getExt(profileImg.getOriginalFilename());
+
+        String fileName = UUID.randomUUID() + "." + ext;
         String profileImgDirPath = genFileDirPath + "/" + profileImgDirName;
         String profileImgFilePath = profileImgDirPath + "/" + fileName;
 
